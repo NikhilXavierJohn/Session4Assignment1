5910d30bd5e3de8e44d0925da6c5597df7126869
NikhilXavierJohn <nkh.john@gmail.com>
2016-06-26 16:04:34 +0530

    Create Session4Assignment1.java

---
+++ Session4Assignment1.java
@@ -0,0 +1,8 @@
+public class Session4Assignmnt1 {
+   public static void main(String[] args) {
+    	int var[] = new int[] {1,2,3,4,5,6,7};
+    	for(int i = var.length - 1; i >= 0 ; i--){
+    		System.out.println(var[i]);
+      }
+    }
+}
